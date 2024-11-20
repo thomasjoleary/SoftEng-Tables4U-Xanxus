f0d640e1befc2b8d66e43418a54c988e10279263
chooboy <chooboy05@gmail.com>
2024-11-20 13:23:07 -0500

    Emptied out project
    Deleted most of the code that was for IP2, should be ready to start coding for real now.

--- src/README.md
+++
@@ -1,7 +0,0 @@
-IP2 - Thomas O'Leary
-
-To run:
-    "npm run dev"
-
-To test with coverage:
-    "npm run test -- --coverage"
--- src/model.test.ts
+++ src/model.test.ts
@@ -2,29 +2,5 @@ import { expect, test } from 'vitest'
 import { Model } from './model'
 
 test('Home', async () => {
-        let m:Model = new Model(0)     // start with first configuration
-        expect(m.board.letters[0][0]).toBe("E")
-        expect(m.board.letters[0][4]).toBe("C")
-        expect(m.board.letters[0][3]).toBe("Y")
-
-        m.board.setSelectSquare(0,4)
-        m.board.checkReady(0,3)
-        m.mergeContents(0,4,0,3)
-        expect(m.board.letters[0][4]).toBe("")
-        expect(m.board.letters[0][3]).toBe("CY")
-        m.board.setSelectSquare(0,3)
-        m.board.checkReady(0,4)
-        m.board.checkReady(1,3)
-        m.board.checkReady(2,4)
-        m.mergeContents(0,3,1,3)
-        expect(m.board.letters[0][3]).toBe("")
-        expect(m.board.letters[1][3]).toBe("CYA")
-
-        m.reset()
-        expect(m.board.letters[0][0]).toBe("E")
-        expect(m.board.letters[0][4]).toBe("C")
-        expect(m.board.letters[0][3]).toBe("Y")
-        m.readyForCheck()
-        m.check()
     }
 )
