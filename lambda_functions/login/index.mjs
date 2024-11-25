478fbcc80b97a1f7dc1dc30deb602bcf1304b7df
chooboy <chooboy05@gmail.com>
2024-11-25 02:45:22 -0500

    Login full pipeline coded. Login Lambda added.
    I coded the front-to-back pipeline for the login use case. To log in as a restaurant, use the restaurant's rid. To log in as an admin, the admin code is "nimda". I also added the lambda function on aws to the lambda_functions folder.

---
+++ lambda_functions/login/index.mjs
@@ -0,0 +1,90 @@
+import pkg from "aws-lambda";
+const { APIGatewayProxyHandler } = pkg;
+import mysql from 'mysql2/promise';
+import { v4 as uuidv4 } from 'uuid';
+
+const dbConfig = {
+    host: "restaurantdb.czcukuq6u2x1.us-east-2.rds.amazonaws.com",
+    user: "xanxus",
+    password: "XanxusRest",
+    database: "xanxus",
+    port: 3306,
+};
+
+export const handler = async (event) => {
+    try {
+        console.log("Function started");
+        console.log(event);
+        const { rid } = JSON.parse(JSON.stringify(event.body || "{}"));
+        console.log(`Received data: rid=${rid}`);
+
+        // if no rid given, return error
+        if (!rid) {
+            return {
+                statusCode: 400,
+                body: JSON.stringify({ error: "Missing 'rid'" }),
+            };
+        }
+        let admin = "no"
+        let success = "false"
+
+        // check for admin
+        if (rid === "nimda") {
+            admin = "yes"
+            success = "true"
+            const active = "true"
+            return {
+                statusCode: 200,
+                body: JSON.stringify({
+                    rid,
+                    success,
+                    admin,
+                    active,
+                }),
+            }
+        }
+
+        // establish database connection
+        const connection = await mysql.createConnection(dbConfig);
+        console.log("Database connection established");
+
+        // query database for given rid
+        const query = `SELECT * FROM Restaurants WHERE rid = '${rid}';`;
+        console.log(`Executing query: ${query} with rid value: ${rid}`);
+        const [restRow] = await connection.execute(query, [rid])
+        
+        
+        if (restRow.length === 1) {
+            // if the restaurant exists
+            console.log("Restaurant exists")
+            success = "true"
+        } else {
+            // if the restaurant doesn't exist
+            return {
+                statusCode: 400,
+                body: JSON.stringify({ error: "Restaurant with that rid does not exist." }),
+            };
+        }
+
+        const active = restRow[0].activated
+        await connection.end();
+        console.log("Database operation complete");
+
+        // return rid, success (true), admin (no), active
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                rid,
+                success,
+                admin,
+                active,
+            }),
+        };
+    } catch (error) {
+        console.error("Error logging in:", error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: "Internal Server Error" }),
+        };
+    }
+};
